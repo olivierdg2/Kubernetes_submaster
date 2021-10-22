40c9dcdacf96c2bcfb98547b516c1fffef096e9d
olivierdg2 <16027@ecam.be>
2021-10-22 15:12:00 +0200

    add todo helpers output

--- kubebuilder/controllers/helpers.go
+++ kubebuilder/controllers/helpers.go
@@ -12,6 +12,7 @@ func (r *SubmasterReconciler) desiredDeployment(sub k3smasterv1.Submaster) (apps
 	t := true
 	var path corev1.HostPathVolumeSource
 	path.Path = "."
+	//TODO add output volume to access kubeconfig from host 
 	depl := appsv1.Deployment{
 		TypeMeta: metav1.TypeMeta{APIVersion: appsv1.SchemeGroupVersion.String(), Kind: "Deployment"},
 		ObjectMeta: metav1.ObjectMeta{
@@ -34,12 +35,14 @@ func (r *SubmasterReconciler) desiredDeployment(sub k3smasterv1.Submaster) (apps
 							Args:            []string{"server", "--kubelet-arg", "port=10350", "--kubelet-arg", "healthz-port=10348", "--kube-proxy-arg", "healthz-bind-address=0.0.0.0:10356", "--kube-proxy-arg", "metrics-bind-address=127.0.0.1:10349", "--kube-scheduler-arg", "port=10351"},
 							Env:             []corev1.EnvVar{{Name: "K3S_TOKEN", Value: "f7b5d6eab0aa000d7a8615065a40e40b"}, {Name: "K3S_KUBECONFIG_OUTPUT", Value: "/output/kubeconfig.yaml"}, {Name: "K3S_KUBECONFIG_MODE", Value: "666"}},
 							VolumeMounts:    []corev1.VolumeMount{{MountPath: "/var/lib/rancher/k3s", Name: "k3s-server"}},
+							//Add mount to output
 							SecurityContext: &corev1.SecurityContext{Privileged: &t},
 						},
 					},
 					HostNetwork: true,
 					//default = emptydir (will be deprecated)
 					Volumes:  []corev1.Volume{{Name: "k3s-server"}},
+					//Add volume to ouput 
 					NodeName: sub.Name,
 				},
 			},
